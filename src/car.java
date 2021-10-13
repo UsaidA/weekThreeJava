61ab1a399cf087d8b25886a7b2affd04038ed72d
Usaid Ahmed <85448432+UsaidA@users.noreply.github.com>
2021-10-13 09:17:22 +0100

    Initial commit

---
+++ src/car.java
@@ -0,0 +1,17 @@
+public class car {
+    private String registrationNumber;
+    private String make;
+
+    private double price;
+
+
+    public void addCar(){
+
+    }
+    public void readList(){
+
+    }
+    public void writeList(){
+
+    }
+}
