d381cf3473f1320080ec78344e99405c5ae57fae
Usaid Ahmed <85448432+UsaidA@users.noreply.github.com>
2021-10-13 10:22:45 +0100

    Making car class

--- src/car.java
+++ src/car.java
@@ -1,11 +1,36 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class car {
     private String registrationNumber;
     private String make;
 
     private double price;
 
+    public car(String registrationNumber,String make, double price){
+        this.registrationNumber = registrationNumber;
+        this.make = make;
+        this.price = price;
 
-    public void addCar(){
+    }
+
+    public String addCar(){
+        System.out.println("\nNew car:\nRegistrationNumber: ");
+        Scanner carReg = new Scanner(System.in);
+        String userCarReg = carReg.next();
+        System.out.println("\nMake: ");
+        Scanner carMake = new Scanner(System.in);
+        String userCarMake = carMake.next();
+        System.out.println("\nPrice: ");
+        Scanner carPrice = new Scanner(System.in);
+        int userCarPrice = carPrice.nextInt();
+        registrationNumber = userCarReg;
+        make = userCarMake;
+        price = userCarPrice;
+        
+        return registrationNumber;
+        return make;
+        return price;
 
     }
     public void readList(){
--- src/main.java
+++ src/main.java
@@ -1,10 +1,32 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 
 public class main {
     public static void main(String[] args){
+    File mp3File = new File("C:\\Users\\bb101\\IdeaProjects\\weekThreeJava\\files\\Me.mp3");
+    File mp3FileChange= new File("C:\\Users\\bb101\\IdeaProjects\\weekThreeJava\\files\\Mine.mp3");
+    File carsFile =  new File("cars.txt");
 
+    List<car> carList = new ArrayList<>();
+    car carOne = new car("BOND007", "AUDI", 50000.0);
+    carList.add(carOne);
+
+
+
+
+
+
+
+
+ /*   if (mp3FileChange.exists())
+    {
+        throw new java.io.IOException("file exists");
+
+    }
+*/
     }
 }
